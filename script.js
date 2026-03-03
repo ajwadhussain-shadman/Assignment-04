ad2e36d29be7590a7e430e5fa77158da527ac85e
ajwadhussain-shadman <ajwadhussain38540@gmail.com>
2026-03-03 16:51:02 +0600

    fixed some code

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const sections = {
    interviewSection: document.getElementById('interview-section'),
    rejectedSection: document.getElementById('rejected-section')
 };
-const nojobSection=document.getElementById('nojob-section');
-let currentJob=document.getElementById('current-job');
+const nojobSection = document.getElementById('nojob-section');
+let currentJob = document.getElementById('current-job');
 function updateCurrentJob() {
    if (cState === 'all') {
       currentJob.innerText = sections.allSection.children.length
@@ -44,69 +44,70 @@ function currentState(id) {
    }
    if (id === 'all') {
       sections.allSection.classList.remove('hidden');
-      if(sections.allSection.children.length <1){
+      if (sections.allSection.children.length < 1) {
          nojobSection.classList.remove('hidden');
       }
-      cState='all';
+      cState = 'all';
    }
    else if (id === 'interview') {
       sections.interviewSection.classList.remove('hidden');
-      if(sections.interviewSection.children.length <1){
+      if (sections.interviewSection.children.length < 1) {
          nojobSection.classList.remove('hidden');
       }
-      cState='interview';
+      cState = 'interview';
    }
    else {
       sections.rejectedSection.classList.remove('hidden');
-       if(sections.rejectedSection.children.length <1){
+      if (sections.rejectedSection.children.length < 1) {
          nojobSection.classList.remove('hidden');
       }
-        cState='rejected';
+      cState = 'rejected';
    }
    count();
    updateCurrentJob();
 }
 
-const totalJob=document.getElementById('total-job');
-const interviewedJob=document.getElementById('interviewed-job');
-const rejectedJob=document.getElementById('rejected-job');
+const totalJob = document.getElementById('total-job');
+const interviewedJob = document.getElementById('interviewed-job');
+const rejectedJob = document.getElementById('rejected-job');
 
-function count(){
-   totalJob.innerText=sections.allSection.children.length+sections.interviewSection.children.length+sections.rejectedSection.children.length;
-interviewedJob.innerText=sections.interviewSection.children.length;
-rejectedJob.innerText=sections.rejectedSection.children.length;
+function count() {
+   totalJob.innerText = sections.allSection.children.length + sections.interviewSection.children.length + sections.rejectedSection.children.length;
+   interviewedJob.innerText = sections.interviewSection.children.length;
+   rejectedJob.innerText = sections.rejectedSection.children.length;
 }
 currentState('all');
 
 
-const buttons=document.querySelectorAll('.interview-btn,.rejected-btn,.delete-btn');
+const buttons = document.querySelectorAll('.interview-btn,.rejected-btn,.delete-btn');
 for (const btn of buttons) {
-   btn.addEventListener('click',function(event){
-      
-      const clickedBtn=event.target;
-      const card=clickedBtn.closest('.card');
-      const statusBox=card.querySelector('.status-box');
+   btn.addEventListener('click', function (event) {
 
-      
-      if(clickedBtn.classList.contains('interview-btn')){
+      const clickedBtn = event.target;
+      const card = clickedBtn.closest('.card');
+      const statusBox = card.querySelector('.status-box');
+
+
+      if (clickedBtn.classList.contains('interview-btn')) {
          sections.interviewSection.appendChild(card);
-         statusBox.classList.remove('text-[#002C5C]','bg-[#EEF4FF]');
-         statusBox.classList.add('border','border-green-600','bg-green-300','text-white');
-         statusBox.innerText='Interview';
-         card.classList.add('border-l-5','border-green-500');
-        currentState(cState);
-      }
-      else if(clickedBtn.classList.contains('rejected-btn')){
-         sections.rejectedSection.appendChild(card);
-         statusBox.classList.remove('text-[#002C5C]','bg-[#EEF4FF]');
-         statusBox.classList.add('border','border-red-600','bg-red-400','text-white');
-         statusBox.innerText='Rejected';
-         card.classList.add('border-l-5','border-red-500');
+         statusBox.classList.remove('text-[#002C5C]', 'bg-[#EEF4FF]', 'border-red-600', 'bg-red-500');
+         statusBox.classList.add('border', 'border-green-600', 'bg-green-500', 'text-white', 'font-extrabold');
+         statusBox.innerText = 'Interview';
+         card.classList.add('border-l-5', 'border-green-500');
+         card.classList.remove('border-red-500');
          currentState(cState);
       }
-      else if(clickedBtn.classList.contains('delete-btn')){
+      else if (clickedBtn.classList.contains('rejected-btn')) {
+         sections.rejectedSection.appendChild(card);
+         statusBox.classList.remove('text-[#002C5C]', 'bg-[#EEF4FF]', 'border-green-600', 'bg-green-500');
+         statusBox.classList.add('border', 'border-red-600', 'bg-red-500', 'text-white');
+         statusBox.innerText = 'Rejected';
+         card.classList.add('border-l-5', 'border-red-500');
+         currentState(cState);
+      }
+      else if (clickedBtn.classList.contains('delete-btn')) {
          card.remove();
-          currentState(cState);
+         currentState(cState);
       }
 
    })
